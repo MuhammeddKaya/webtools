415575dfb1c266bec081eed52fe84a587252fd70
MuhammeddKaya <muhammedkaya0476@gmail.com>
2026-03-08 11:26:23 +0300

    update

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -6,6 +6,7 @@ from .models import StaticPage
 @admin.register(StaticPage)
 class StaticPageAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "is_active", "show_in_nav", "show_in_footer")
+    list_editable = ("is_active", "show_in_nav", "show_in_footer")
     list_filter = ("is_active", "show_in_nav", "show_in_footer")
     search_fields = ("title", "slug", "key")
     ordering = ("nav_order", "title")
